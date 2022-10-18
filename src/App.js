4d3493308df853407c0c619a68929b8a1e2edd3d
criseria <criseria4212@naver.com>
2022-10-18 21:18:12 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+// import ControlledComponent from './components/2-10.Form/ControlledComponent';
+// import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
+// import List from './components/2-9.List/List'
+// import Condition from './components/2-8.ConditionalRendering/Condition';
+// import Event from './components/2-7.Event/Event';
+// import Composition from './components/2-4.Props/Composition';
+// import Extraction from './components/2-4.Props/Extraction/Extraction';
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import FuntionalComponent from './components/2-5.State/FuntionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <UncontrolledComponent /> */}
+      {/* <ControlledComponent /> */}
+      {/* <List /> */}
+      {/* <Condition /> */}
+      {/* <Event /> */}
+      {/* <FuntionalComponent /> */}
+      {/* <ClassComponent /> */}
+      {/* <Composition /> */}
+      {/* <Extraction /> */}
     </div>
   );
 }
